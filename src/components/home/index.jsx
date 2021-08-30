2bf7c4ab08fe92769ae5d48cbe2770df13b56c1f
Andres Arana <ajoelarana@gmail.com>
2021-08-29 23:29:13 -0400

    Home product summaries

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -17,26 +17,46 @@ class Home extends Component {
         <header>
           <h1>Build the perfect gift</h1>
 
-          <p>Whether you're celebrating a special occasion or showing someone how much you appreciate them, BundleGrove will help you make the perfect gift. Whether it's a basket of desserts, a breakfast basket, or a movie night basket, we'll customize it to include what you want.</p>
+          <p>Whether you're celebrating a special occasion or showing someone how much you appreciate them, BundleGrove will help you make the perfect gift.</p>
         </header>
 
         <section>
-          <FontAwesomeIcon 
-            icon={['fas', 'recycle']}
-          />
-          Reuse our baskets and paper decorations
-          <FontAwesomeIcon
-            icon={['fas', 'gift']}
-          />
-          Show your appreciation
-          <FontAwesomeIcon
-            icon={['fas', 'coffee']}
-          />
-          Give a breakfast basket
-          <FontAwesomeIcon
-            icon={['fas', 'shopping-basket']}
-          />
-          Customize
+          <ul className="product-summary-list">
+            <li
+              className="premade-item"
+            >    
+              <section className="product-summary-copy">
+                <h2><FontAwesomeIcon
+                  icon={['fas', 'gift']}
+                /> Premade bundles</h2>
+
+                <p>Gift one of our premade bundles with a personalized note!</p>
+              </section>
+            </li>
+
+            <li
+              className="customizable-item"
+            >
+              <section className="product-summary-copy">
+                <h2><FontAwesomeIcon
+                  icon={['fas', 'shopping-basket']}
+                /> Customize your own bundle</h2>
+
+                <p>Build your perfect bundle by selecting what you want.</p>
+              </section>
+            </li>
+
+            <li
+              className="green-item"
+            >
+              <section className="product-summary-copy">
+                <h2><FontAwesomeIcon
+                  icon={['fas', 'recycle']}
+                /> Reuse and recycle bundle materials</h2>
+                <p>We encourage everyone to either reuse or recycle our bundle boxes and decorations!</p>
+              </section>
+            </li>
+          </ul>
         </section>
       </article>
     )
