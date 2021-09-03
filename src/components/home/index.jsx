fffcc4b347b2acf0b7db8c78c1d9759d4aa7a2f1
Andres Arana <ajoelarana@gmail.com>
2021-09-03 00:24:13 -0400

    wip

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,6 +5,9 @@ import basket1 from '../../assets/baskets/basket-1.jpg';
 import basket2 from '../../assets/baskets/basket-2.jpg';
 import basket3 from '../../assets/baskets/basket-3.jpg';
 import Button from '../button';
+import {
+  NavLink
+} from 'react-router-dom';
 
 class Home extends Component {
   constructor(props) {
@@ -19,7 +22,7 @@ class Home extends Component {
     return (
       <article className="home-page">
         <header className="home-page-header">
-          <h1>Build the perfect gift</h1>
+          <h1>A gift bundle for every occasion</h1>
 
           <p>Whether you're celebrating a special occasion or showing someone how much you appreciate them, BundleGrove will help you make the perfect gift.</p>
 
@@ -42,11 +45,20 @@ class Home extends Component {
                   />
                 </header>
 
-                <h2><FontAwesomeIcon
-                  icon={['fas', 'gift']}
-                />Premade bundles</h2>
+                <h2>
+                  {/* <FontAwesomeIcon
+                    icon={['fas', 'gift']}
+                  /> */}
+                  Premade bundles
+                </h2>
 
-                <p>Gift one of our premade bundles with a personalized note!</p>
+                <p>Look through our premade bundles to decide which one is right for you. Add a personalized note at no additional charge.</p>
+
+                <NavLink 
+                  to={"/"}
+                >
+                  View Premade Bundles
+                </NavLink>
               </section>
             </li>
 
@@ -60,9 +72,12 @@ class Home extends Component {
                   />
                 </header>
 
-                <h2><FontAwesomeIcon
-                  icon={['fas', 'shopping-basket']}
-                />Customize your own bundle</h2>
+                <h2>
+                  {/* <FontAwesomeIcon
+                    icon={['fas', 'shopping-basket']}
+                  /> */}
+                  Customize your own bundle
+                </h2>
 
                 <p>Build your perfect bundle by selecting what you want.</p>
               </section>
@@ -78,9 +93,12 @@ class Home extends Component {
               </header>
 
               <section className="product-summary-copy">
-                <h2><FontAwesomeIcon
-                  icon={['fas', 'recycle']}
-                />Reuse and recycle bundle materials</h2>
+                <h2>
+                  {/* <FontAwesomeIcon
+                    icon={['fas', 'recycle']}
+                  /> */}
+                  Reuse and recycle bundle materials
+                </h2>
                 <p>We encourage everyone to either reuse or recycle our bundle boxes and decorations!</p>
               </section>
             </li>
